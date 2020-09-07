83f03adb7c80d202ff56ec8d7e4acab0d283d3d9
Bernard Mordan <bernardmordan@gmail.com>
2020-09-07 16:04:43 +0100

    all the things in version control

--- main.js
+++ main.js
@@ -15,3 +15,73 @@ function are_equal(arr_a, arr_b) {
     part1.concat(part2.reverse()).toString() === arr_a.toString()
 }
 are_equal(arr_a, arr_b)
+
+class Note {
+	constructor(freq) {
+		this.freq = freq
+		this.selected = false
+		this.el = $("<samp></samp>")
+		this.el.on('click', this.toggle.bind(this))
+		this.renderSelected()
+	}
+	toggle (evt) {
+		this.selected = !this.selected
+		this.renderSelected()
+	}
+	renderSelected () {
+		this.el.removeClass(["selected", "unselected"])
+		this.el.addClass(this.selected ? "selected" : "unselected")
+	}
+}
+
+class Grid {
+	constructor() {
+		this.worker = new Worker("/worker.js")
+		this.bar = 0
+		this.grid = [
+			[1047.0, 1175, 1319, 1397, 1480, 1568, 1760, 1976],
+			[523.3, 587.3, 659.3, 698.5, 740, 784, 880, 987.8],
+			[261.6, 293.7, 329.6, 349.2, 370, 392, 440, 493.9],
+			[130.8, 146.8, 164.8, 174.6, 185, 196, 220, 246.9]
+		].map(row => row.map(freq => new Note(freq)))
+		this.worker.onmessage = this._tick.bind(this)
+	}
+
+	play() {
+		this.worker.postMessage("start")
+	}
+
+	stop() {
+		this.worker.postMessage("stop")
+		this.grid.forEach(row => row.forEach(note => {
+			note.selected = false
+			note.renderSelected()
+		}))
+		this.bar = 0
+	}
+
+	_tick() {
+		this._play()
+		this.bar = this.bar === 7 ? 0 : this.bar + 1
+	}
+}
+
+function AppAudio () {}
+AppAudio.prototype.context = undefined
+AppAudio.prototype.setContext = function() {
+	this.context = new AudioContext()
+}
+
+const Note = function (freq) {
+	if (!this.constructor.context) this.constructor.setContext()
+	const o = this.constructor.context.createOscillator()
+	o.frequency.value = freq
+	o.connect(this.constructor.context.destination)
+	o.start()
+	o.stop(this.constructor.context.currentTime + 1)
+}
+Note.prototype = Object.create(new AppAudio())
+
+function play (freq) {
+	new Note(freq)
+}
